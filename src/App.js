31a2d09167c0630054c022674c7e465f56d270a4
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2022-07-27 01:26:07 +0600

    just for push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Translate from "./utils/translate";
-// {Translate("bn", "common.login")}
+import {Navigate,Route,Routes} from 'react-router-dom'
 
 const App = () => {
-  return <div className="App"></div>;
+  return <div className="App">{Translate("bn", "common.login")}</div>;
 };
 
 export default App;
